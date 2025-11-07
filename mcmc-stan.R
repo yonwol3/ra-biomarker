0ed8d2c51ff0657ccf28d793be565f29443fa99d
kevjosey <kjosey@hsph.harvard.edu>
2025-11-06 20:39:55 -0700

    Rerunning one more time. Need to update readme.

--- mcmc-stan.R
+++ mcmc-stan.R
@@ -18,44 +18,47 @@ a <- b <- rep(0, times = K)
 R <- S <- diag(1e8, nrow = K, ncol = K) # beta/mu covariance hyperparameters
 
 standata_A <- list(N = N, M = M, K = K, Y = Y, L = L, U = U, D = D,
-                 t = time, g = diagnosis, id = subj_id, a = a, b = b, S = S, R = R)
+                 t = time, g = diagnosis, id = study_id, a = a, b = b, S = S, R = R)
 
 # Truncation at LoD
-stanmodel_trunc_A <- stan_model(file = "~/Github/ra-biomarker/stan/change-point-trunc.stan", model_name = "stanmodel_trunc_A")
+stanmodel_trunc_A <- stan_model(file = "~/Github/ra-biomarker/stan/mixture-trunc.stan", model_name = "stanmodel_trunc_A")
 samples_trunc_A <- sampling(stanmodel_trunc_A, data = standata_A, iter = 10000, warmup = 5000, 
                           chains = 4, thin = 10, check_data = FALSE, cores = 4,
                           control = list(max_treedepth = 15))
 mcmc_trunc_A <- do.call(cbind, rstan::extract(samples_trunc_A, 
-                                            pars = c(paste0("gamma[", 1:K, "]"), 
-                                                     paste0("kappa[", 1:K, "]")), 
-                                            permuted = TRUE))
+                                              pars = c(paste0("gamma[", 1:K, "]"), 
+                                                       paste0("delta[", 1:K, "]"),
+                                                       paste0("phi[", 1:K, "]")), 
+                                              permuted = TRUE))
 save(mcmc_trunc_A, file = "mcmc/mcmc_trunc_A.RData")
 summary(mcmc_trunc_A)
 
 # Censoring Above LoD
-stanmodel_cens_A <- stan_model(file = "~/Github/ra-biomarker/stan/change-point-cens.stan", model_name = "stanmodel_cens_A")
+stanmodel_cens_A <- stan_model(file = "~/Github/ra-biomarker/stan/mixture-cens.stan", model_name = "stanmodel_cens_A")
 samples_cens_A <- sampling(stanmodel_cens_A, data = standata_A, iter = 10000, warmup = 5000, 
                          chains = 4, thin = 10, check_data = FALSE, cores = 4, 
                          control = list(max_treedepth = 15))
 mcmc_cens_A <- do.call(cbind, rstan::extract(samples_cens_A, 
-                                            pars = c(paste0("gamma[", 1:K, "]"), 
-                                                     paste0("kappa[", 1:K, "]")), 
+                                             pars = c(paste0("gamma[", 1:K, "]"), 
+                                                      paste0("delta[", 1:K, "]"),
+                                                      paste0("phi[", 1:K, "]")), 
                                             permuted = TRUE))
 save(mcmc_cens_A, file = "mcmc/mcmc_cens_A.RData")
 summary(mcmc_cens_A)
 
 # Binarize Detection
 standata_bin_A <- list(N = N, M = M, K = K, Y = Y_bin,
-                 t = time, g = diagnosis, id = subj_id,
+                 t = time, g = diagnosis, id = study_id,
                  a = a, b = b, S = S, R = R)
 
-stanmodel_bin_A <- stan_model(file = "~/Github/ra-biomarker/stan/change-point-bin.stan", model_name = "stanmodel_bin_A")
+stanmodel_bin_A <- stan_model(file = "~/Github/ra-biomarker/stan/mixture-bin.stan", model_name = "stanmodel_bin_A")
 samples_bin_A <- sampling(stanmodel_bin_A, data = standata_bin_A, iter = 10000, warmup = 5000, 
                           chains = 4, thin = 10, check_data = FALSE, cores = 4,
                           control = list(max_treedepth = 15))
 mcmc_bin_A <- do.call(cbind, rstan::extract(samples_bin_A, 
                                             pars = c(paste0("gamma[", 1:K, "]"), 
-                                                     paste0("kappa[", 1:K, "]")), 
+                                                     paste0("delta[", 1:K, "]"),
+                                                     paste0("phi[", 1:K, "]")), 
                                             permuted = TRUE))
 save(mcmc_bin_A, file = "mcmc/mcmc_bin_A.RData")
 summary(mcmc_bin_A)
@@ -68,30 +71,32 @@ a <- b <- rep(0, times = K)
 R <- S <- diag(1e8, nrow = K, ncol = K) # beta/mu covariance hyperparameters
 
 standata_B <- list(N = N, M = M, K = K, Y = Y, L = L, U = U, D = D,
-                     t = time, g = diagnosis, id = subj_id, 
+                     t = time, g = diagnosis, id = study_id, 
                      a = a, b = b, S = S, R = R)
 
 # Truncation at LoD
-stanmodel_trunc_B <- stan_model(file = "~/Github/ra-biomarker/stan/change-point-trunc.stan", model_name = "stanmodel_trunc_B")
+stanmodel_trunc_B <- stan_model(file = "~/Github/ra-biomarker/stan/mixture-trunc.stan", model_name = "stanmodel_trunc_B")
 samples_trunc_B <- sampling(stanmodel_trunc_B, data = standata_B, iter = 10000, warmup = 5000,
                             chains = 4, thin = 10, check_data = FALSE, cores = 4,
                             control = list(max_treedepth = 15))
 mcmc_trunc_B <- do.call(cbind, rstan::extract(samples_trunc_B, 
-                                                pars = c(paste0("gamma[", 1:K, "]"), 
-                                                         paste0("kappa[", 1:K, "]")), 
-                                                permuted = TRUE))
+                                              pars = c(paste0("gamma[", 1:K, "]"), 
+                                                       paste0("delta[", 1:K, "]"),
+                                                       paste0("phi[", 1:K, "]")), 
+                                              permuted = TRUE))
 save(mcmc_trunc_B, file = "mcmc/mcmc_trunc_B.RData")
 summary(mcmc_trunc_B)
 
 # Censoring above LoD
-stanmodel_cens_B <- stan_model(file = "~/Github/ra-biomarker/stan/change-point-cens.stan", model_name = "stanmodel_cens_B")
+stanmodel_cens_B <- stan_model(file = "~/Github/ra-biomarker/stan/mixture-cens.stan", model_name = "stanmodel_cens_B")
 samples_cens_B <- sampling(stanmodel_cens_B, data = standata_B, iter = 10000, warmup = 5000, 
                            chains = 4, thin = 10, check_data = TRUE, cores = 4,
                            control = list(max_treedepth = 15))
 mcmc_cens_B <- do.call(cbind, rstan::extract(samples_cens_B, 
-                                              pars = c(paste0("gamma[", 1:K, "]"), 
-                                                       paste0("kappa[", 1:K, "]")), 
-                                              permuted = TRUE))
+                                             pars = c(paste0("gamma[", 1:K, "]"), 
+                                                      paste0("delta[", 1:K, "]"),
+                                                      paste0("phi[", 1:K, "]")), 
+                                             permuted = TRUE))
 save(mcmc_cens_B, file = "mcmc/mcmc_cens_B.RData")
 summary(mcmc_cens_B)
 
@@ -100,14 +105,15 @@ standata_bin_B <- list(N = N, M = M, K = K, Y = Y_bin,
                          t = time, g = diagnosis, id = study_id, 
                          a = a, b = b, S = S, R = R)
 
-stanmodel_bin_B <- stan_model(file = "~/Github/ra-biomarker/stan/change-point-bin.stan", model_name = "stanmodel_bin_B")
+stanmodel_bin_B <- stan_model(file = "~/Github/ra-biomarker/stan/mixture-bin.stan", model_name = "stanmodel_bin_B")
 samples_bin_B <- sampling(stanmodel_bin_B, data = standata_bin_B, iter = 10000, warmup = 5000, 
                         chains = 4, thin = 10, check_data = TRUE, cores = 4,
                         control = list(max_treedepth = 15))
 mcmc_bin_B <- do.call(cbind, rstan::extract(samples_bin_B, 
-                                              pars = c(paste0("gamma[", 1:K, "]"), 
-                                                       paste0("kappa[", 1:K, "]")), 
-                                              permuted = TRUE))
+                                            pars = c(paste0("gamma[", 1:K, "]"), 
+                                                     paste0("delta[", 1:K, "]"),
+                                                     paste0("phi[", 1:K, "]")), 
+                                            permuted = TRUE))
 save(mcmc_bin_B, file = "mcmc/mcmc_bin_B.RData")
 summary(mcmc_bin_B)
 
--- stan/mixture-cens.stan
+++ stan/mixture-cens.stan
@@ -13,7 +13,7 @@ data {
   vector[K] U[N];
   vector[N] t;
   vector[N] g;
-  vector<lower=L, upper=U>[K] Y[N];
+  vector<lower=L>[K] Y[N];
 
   // hyperparameters
   vector[K] a;
@@ -25,7 +25,7 @@ data {
 
 parameters {
   
-  // mean
+  // mean function
   vector[K] theta;
   vector[K] alpha[M];
   vector[K] beta1;
@@ -33,20 +33,19 @@ parameters {
   vector[K] gamma;
   
   // covariance
-  cholesky_factor_corr[K] corr_e;
   vector<lower=0>[K] sigma_0;
   vector<lower=0>[K] sigma_e;
-  
+
   // changepoint
   vector<lower=-20,upper=10>[K] delta;
+  vector<lower=0>[K] phi; 
 
 }
 
 transformed parameters {
 
   cov_matrix[K] Sigma_0 = diag_matrix(square(sigma_0));
-  matrix[K,K] Sigma_e = diag_pre_multiply(sigma_e, corr_e);
-  
+
 }
 
 model {
@@ -56,26 +55,36 @@ model {
   beta1 ~ multi_normal(b, S);
   beta2 ~ multi_normal(b, S);
   gamma ~ multi_normal(b, S);
-  corr_e ~ lkj_corr_cholesky(1);
   sigma_e ~ cauchy(0, 5);
   sigma_0 ~ cauchy(0, 5);
+  phi ~ cauchy(0, 5);
   delta ~ uniform(-20, 10);
-  
-  // Transformed Data
-  vector[K] mu[N];
 
   // Random Intercept
   alpha ~ multi_normal(theta, Sigma_0);
   
   // Likelihood
-  Y ~ multi_normal_cholesky(mu, Sigma_e);
-
   for (i in 1:N) {
-    
+
     for (k in 1:K) {
-      
-      mu[i,k] = alpha[id[i],k] + beta1[k]*g[i] + beta2[k]*t[i] + gamma[k]*g[i]*fdim(t[i], delta[k]);
-      
+
+      real rho = inv_logit((t[i] - delta[k]) * phi[k]);
+      real eta_0 = alpha[id[i],k] + beta1[k]*g[i] + beta2[k]*t[i];
+      real eta_1 = eta_0 + gamma[k]*g[i]*fdim(t[i],delta[k]);
+      real mu = (1 - rho) * eta_0 + rho * eta_1;
+
+      if (D[i,k] == 0) {
+
+        target += normal_lpdf(Y[i,k] | mu, sigma_e[k]);
+
+      } else if (D[i,k] == 1) {
+
+        real z = (U[i,k] - mu) / sigma_e[k];
+        real p = 1.0 - Phi(z);
+        target += log(fmax(p, 1e-10));  // avoid log(0)
+
+      }
+
     }
     
   }
