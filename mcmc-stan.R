47af82707a78edac1fbbe252b7882df91026a97c
kevjosey <kjosey@hsph.harvard.edu>
2026-03-27 12:07:54 -0600

    Cleaning up for second submission.

--- mcmc-stan.R
+++ mcmc-stan.R
@@ -21,8 +21,8 @@ standata_A <- list(N = N, M = M, K = K, Y = Y, L = L, U = U, D = D,
                  t = time, g = diagnosis, id = study_id, a = a, b = b, S = S, R = R)
 
 # Truncation at LoD
-stanmodel_trunc_A <- stan_model(file = "~/Github/ra-biomarker/stan/change-point-trunc.stan", model_name = "stanmodel_trunc_A")
-samples_trunc_A <- sampling(stanmodel_trunc_A, data = standata_A, iter = 10000, warmup = 5000, 
+stanmodel_trunc_A <- stan_model(file = "~/Github/ra-biomarker/stan/mixture-trunc.stan", model_name = "stanmodel_trunc_A")
+samples_trunc_A <- sampling(stanmodel_trunc_A, data = standata_A, iter = 20000, warmup = 10000, 
                           chains = 4, thin = 10, check_data = FALSE, cores = 4,
                           control = list(max_treedepth = 15))
 mcmc_trunc_A <- do.call(cbind, rstan::extract(samples_trunc_A, 
@@ -33,8 +33,8 @@ save(mcmc_trunc_A, file = "mcmc/mcmc_trunc_A.RData")
 summary(mcmc_trunc_A)
 
 # Censoring Above LoD
-stanmodel_cens_A <- stan_model(file = "~/Github/ra-biomarker/stan/change-point-cens.stan", model_name = "stanmodel_cens_A")
-samples_cens_A <- sampling(stanmodel_cens_A, data = standata_A, iter = 10000, warmup = 5000, 
+stanmodel_cens_A <- stan_model(file = "~/Github/ra-biomarker/stan/mixture-cens.stan", model_name = "stanmodel_cens_A")
+samples_cens_A <- sampling(stanmodel_cens_A, data = standata_A, iter = 20000, warmup = 10000, 
                          chains = 4, thin = 10, check_data = FALSE, cores = 4, 
                          control = list(max_treedepth = 15))
 mcmc_cens_A <- do.call(cbind, rstan::extract(samples_cens_A, 
@@ -43,23 +43,6 @@ mcmc_cens_A <- do.call(cbind, rstan::extract(samples_cens_A,
                                             permuted = TRUE))
 save(mcmc_cens_A, file = "mcmc/mcmc_cens_A.RData")
 summary(mcmc_cens_A)
-
-# Binarize Detection
-standata_bin_A <- list(N = N, M = M, K = K, Y = Y_bin,
-                 t = time, g = diagnosis, id = study_id,
-                 a = a, b = b, S = S, R = R)
-
-stanmodel_bin_A <- stan_model(file = "~/Github/ra-biomarker/stan/mixture-bin.stan", model_name = "stanmodel_bin_A")
-samples_bin_A <- sampling(stanmodel_bin_A, data = standata_bin_A, iter = 10000, warmup = 5000, 
-                          chains = 4, thin = 10, check_data = FALSE, cores = 4,
-                          control = list(max_treedepth = 15))
-mcmc_bin_A <- do.call(cbind, rstan::extract(samples_bin_A, 
-                                            pars = c(paste0("gamma[", 1:K, "]"), 
-                                                     paste0("delta[", 1:K, "]"),
-                                                     paste0("phi[", 1:K, "]")), 
-                                            permuted = TRUE))
-save(mcmc_bin_A, file = "mcmc/mcmc_bin_A.RData")
-summary(mcmc_bin_A)
 
 ### Sample B
 
@@ -73,8 +56,8 @@ standata_B <- list(N = N, M = M, K = K, Y = Y, L = L, U = U, D = D,
                      a = a, b = b, S = S, R = R)
 
 # Truncation at LoD
-stanmodel_trunc_B <- stan_model(file = "~/Github/ra-biomarker/stan/change-point-trunc.stan", model_name = "stanmodel_trunc_B")
-samples_trunc_B <- sampling(stanmodel_trunc_B, data = standata_B, iter = 10000, warmup = 5000,
+stanmodel_trunc_B <- stan_model(file = "~/Github/ra-biomarker/stan/mixture-trunc.stan", model_name = "stanmodel_trunc_B")
+samples_trunc_B <- sampling(stanmodel_trunc_B, data = standata_B, iter = 20000, warmup = 10000,
                             chains = 4, thin = 10, check_data = FALSE, cores = 4,
                             control = list(max_treedepth = 15))
 mcmc_trunc_B <- do.call(cbind, rstan::extract(samples_trunc_B, 
@@ -85,8 +68,8 @@ save(mcmc_trunc_B, file = "mcmc/mcmc_trunc_B.RData")
 summary(mcmc_trunc_B)
 
 # Censoring above LoD
-stanmodel_cens_B <- stan_model(file = "~/Github/ra-biomarker/stan/change-point-cens.stan", model_name = "stanmodel_cens_B")
-samples_cens_B <- sampling(stanmodel_cens_B, data = standata_B, iter = 10000, warmup = 5000, 
+stanmodel_cens_B <- stan_model(file = "~/Github/ra-biomarker/stan/mixture-cens.stan", model_name = "stanmodel_cens_B")
+samples_cens_B <- sampling(stanmodel_cens_B, data = standata_B, iter = 20000, warmup = 10000, 
                            chains = 4, thin = 10, check_data = TRUE, cores = 4,
                            control = list(max_treedepth = 15))
 mcmc_cens_B <- do.call(cbind, rstan::extract(samples_cens_B, 
@@ -95,21 +78,4 @@ mcmc_cens_B <- do.call(cbind, rstan::extract(samples_cens_B,
                                              permuted = TRUE))
 save(mcmc_cens_B, file = "mcmc/mcmc_cens_B.RData")
 summary(mcmc_cens_B)
-
-# Binarize Detection
-standata_bin_B <- list(N = N, M = M, K = K, Y = Y_bin,
-                         t = time, g = diagnosis, id = study_id, 
-                         a = a, b = b, S = S, R = R)
-
-stanmodel_bin_B <- stan_model(file = "~/Github/ra-biomarker/stan/mixture-bin.stan", model_name = "stanmodel_bin_B")
-samples_bin_B <- sampling(stanmodel_bin_B, data = standata_bin_B, iter = 10000, warmup = 5000, 
-                        chains = 4, thin = 10, check_data = TRUE, cores = 4,
-                        control = list(max_treedepth = 15))
-mcmc_bin_B <- do.call(cbind, rstan::extract(samples_bin_B, 
-                                            pars = c(paste0("gamma[", 1:K, "]"), 
-                                                     paste0("delta[", 1:K, "]"),
-                                                     paste0("phi[", 1:K, "]")), 
-                                            permuted = TRUE))
-save(mcmc_bin_B, file = "mcmc/mcmc_bin_B.RData")
-summary(mcmc_bin_B)
 
