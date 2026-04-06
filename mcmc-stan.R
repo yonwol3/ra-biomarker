edb406e690df3fe17b0bdc8fea688f9c427ff01d
kevjosey <kjosey@hsph.harvard.edu>
2026-04-06 07:50:43 -0600

    something.

--- mcmc-stan.R
+++ mcmc-stan.R
@@ -21,9 +21,9 @@ standata_A <- list(N = N, M = M, K = K, Y = Y, L = L, U = U, D = D,
                  t = time, g = diagnosis, id = study_id, a = a, b = b, S = S, R = R)
 
 # Truncation at LoD
-stanmodel_trunc_A <- stan_model(file = "~/Github/ra-biomarker/stan/mixture-trunc.stan", model_name = "stanmodel_trunc_A")
+stanmodel_trunc_A <- stan_model(file = "~/Github/ra-biomarker/stan/change-point-trunc.stan", model_name = "stanmodel_trunc_A")
 samples_trunc_A <- sampling(stanmodel_trunc_A, data = standata_A, iter = 20000, warmup = 10000, 
-                          chains = 4, thin = 10, check_data = FALSE, cores = 4,
+                          chains = 5, thin = 10, check_data = FALSE, cores = 5,
                           control = list(max_treedepth = 15))
 mcmc_trunc_A <- do.call(cbind, rstan::extract(samples_trunc_A, 
                                               pars = c(paste0("gamma[", 1:K, "]"), 
@@ -33,9 +33,9 @@ save(mcmc_trunc_A, file = "mcmc/mcmc_trunc_A.RData")
 summary(mcmc_trunc_A)
 
 # Censoring Above LoD
-stanmodel_cens_A <- stan_model(file = "~/Github/ra-biomarker/stan/mixture-cens.stan", model_name = "stanmodel_cens_A")
+stanmodel_cens_A <- stan_model(file = "~/Github/ra-biomarker/stan/change-point-cens.stan", model_name = "stanmodel_cens_A")
 samples_cens_A <- sampling(stanmodel_cens_A, data = standata_A, iter = 20000, warmup = 10000, 
-                         chains = 4, thin = 10, check_data = FALSE, cores = 4, 
+                         chains = 5, thin = 10, check_data = FALSE, cores = 5, 
                          control = list(max_treedepth = 15))
 mcmc_cens_A <- do.call(cbind, rstan::extract(samples_cens_A, 
                                              pars = c(paste0("gamma[", 1:K, "]"), 
@@ -56,9 +56,9 @@ standata_B <- list(N = N, M = M, K = K, Y = Y, L = L, U = U, D = D,
                      a = a, b = b, S = S, R = R)
 
 # Truncation at LoD
-stanmodel_trunc_B <- stan_model(file = "~/Github/ra-biomarker/stan/mixture-trunc.stan", model_name = "stanmodel_trunc_B")
+stanmodel_trunc_B <- stan_model(file = "~/Github/ra-biomarker/stan/change-point-trunc.stan", model_name = "stanmodel_trunc_B")
 samples_trunc_B <- sampling(stanmodel_trunc_B, data = standata_B, iter = 20000, warmup = 10000,
-                            chains = 4, thin = 10, check_data = FALSE, cores = 4,
+                            chains = 5, thin = 10, check_data = FALSE, cores = 5,
                             control = list(max_treedepth = 15))
 mcmc_trunc_B <- do.call(cbind, rstan::extract(samples_trunc_B, 
                                               pars = c(paste0("gamma[", 1:K, "]"), 
@@ -68,9 +68,9 @@ save(mcmc_trunc_B, file = "mcmc/mcmc_trunc_B.RData")
 summary(mcmc_trunc_B)
 
 # Censoring above LoD
-stanmodel_cens_B <- stan_model(file = "~/Github/ra-biomarker/stan/mixture-cens.stan", model_name = "stanmodel_cens_B")
+stanmodel_cens_B <- stan_model(file = "~/Github/ra-biomarker/stan/change-point-cens.stan", model_name = "stanmodel_cens_B")
 samples_cens_B <- sampling(stanmodel_cens_B, data = standata_B, iter = 20000, warmup = 10000, 
-                           chains = 4, thin = 10, check_data = TRUE, cores = 4,
+                           chains = 5, thin = 10, check_data = TRUE, cores = 5,
                            control = list(max_treedepth = 15))
 mcmc_cens_B <- do.call(cbind, rstan::extract(samples_cens_B, 
                                              pars = c(paste0("gamma[", 1:K, "]"), 
